e3b9245a0e28d848da4fd9a76b97b603baf0f919
Admin <dmitrijdundakov@gmail.com>
2021-10-15 21:43:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 32700;
+        int i = 2_100_200_300;
+        long l = 260235002000000000L;
+        float f = 134.01f;
+        double d = 100.123456;
+        char c = 'a';
+        boolean o = !false;
+        System.out.println(b+"\n"+s+"\n"+i+"\n"+l+"\n"+f+"\n"+d+"\n"+c+"\n"+o);
+            }
+    }
